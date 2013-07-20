a086bed3c387512ff45825edab3a7bc1a8462344
Jace Kohlmeier <jace@khanacademy.org>
2013-07-19 17:35:54 -0700

    add authentication to the webapp

--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,20 @@
+from google.appengine.api import users
+
 import webapp2
 
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
 
+        self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
