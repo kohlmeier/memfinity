6cd3bada33a480420a15ff7a79ce049872f9b385
Sam Birch <sam.m.birch@gmail.com>
2013-07-20 13:38:04 -0700

    basics of card from content script to bg to popup to bg to upload

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1,6 +1,36 @@
+// http://stackoverflow.com/questions/3907804/how-to-detect-when-action-popup-gets-closed
+// unload events don't work for popups.
+function ping() {
+    chrome.extension.getBackgroundPage().PopupCloseMonitor.popupPing();
+    setTimeout(ping, 500);
+}
+ping();
 
-chrome.tabs.getSelected(null, function(tab) {
-  //tab.url, tab.title
-  chrome.tabs.executeScript(null, {file: 'inject.js'});
+function init(card){
+  $('#front').val(card.front);
+  $('#back').val(card.back);
+
+  $('#front,#back').change(function(){
+    card.front = $('#front').val();
+    card.back = $('#back').val()
+    chrome.runtime.sendMessage({
+      origin: 'popup',
+      content: card
+    });
+  });
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log('popup got message', request, sender);
+
+  if (request.origin === 'background'){
+    var card = request.content;
+    console.log(card);
+    init(card);
+  }
 });
 
+// Set everything in motion via the content script
+chrome.tabs.getSelected(null, function(tab) {
+  chrome.tabs.executeScript(null, {file: 'inject.js'});
+});
