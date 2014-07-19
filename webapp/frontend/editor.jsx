5474057b7fa1dac08fc8e90d1ddaf6651b7e6480
Chris Klaiber <cklaiber@gmail.com>
2014-07-19 00:19:27 -0700

    Style editor, hook up jQuery Tags Input plugin
    I moved reset.css into its own file, and re-ordered the CSS imports so
    the cascade can work properly (reset, bootstrap, flat-ui, our CSS).

--- webapp/frontend/editor.jsx
+++ webapp/frontend/editor.jsx
@@ -17,37 +17,71 @@ var EditorForm = React.createClass({
         var state = {};
         if (field === 'reversible' || field === 'public') {
             state[field] = event.target.checked;
-        } else if (field === 'tags') {
-            state[field] = _.map(event.target.value.split(","), function (tag) {
-                return tag.trim();
-            });
-            console.log("Put this in state: ", state[field]);
         } else {
             state[field] = event.target.value;
         }
         this.setState(state);
     },
+    handleTagsInputChange: function(elt) {
+        // The tagsInput arguments aren't helpful or consistent, so we
+        // go straight to the source.
+        // TODO(chris): this is called once per tag on initialization,
+        // which is silly.
+        var tags = this.refs.tagsinput.getDOMNode().value.split(',');
+        this.setState({tags: tags});
+    },
     handleSubmit: function() {
         this.props.submitCardData(this.state);
     },
     render: function() {
-        var tagsCSV = this.props.cardModel.tags.join(", ");
+        var tagsCSV = this.props.cardModel.tags.join(",");
         console.log("cardModel=", this.props.cardModel);
         console.log(tagsCSV);
         return <form className="editorForm" onSubmit={this.handleSubmit}>
-            Front <br/>
-            <textarea className="editor-text" rows="7" cols="400" defaultValue={this.props.cardModel.front} onChange={_.partial(this.handleChange, 'front')} /> <br/>
-            Back <br/>
-            <textarea className="editor-text" rows="7" cols="400" defaultValue={this.props.cardModel.back} onChange={_.partial(this.handleChange, 'back')} /> <br/>
-            Tags <br/>
-            <input type="text" className="editor-text" defaultValue={tagsCSV} onChange={_.partial(this.handleChange, 'tags')} /> <br/>
-            Source URL <br/>
-            <input type="text" className="editor-text" defaultValue={this.props.cardModel.source_url} onChange={_.partial(this.handleChange, 'source_url')} /> <br/>
-            <input type="checkbox" defaultChecked={this.props.cardModel.reversible} onClick={_.partial(this.handleChange, 'reversible')}/> Reversible<br/>
-            <input type="checkbox" defaultChecked={this.props.cardModel.public} onClick={_.partial(this.handleChange, 'public')}/> Public<br/>
-            <br/>
-            <input type="submit" value="Save" />
+            <div className="form-group">
+                <label htmlFor="editor-input-front">Front</label>
+                <textarea id="editor-input-front" className="form-control span6"
+                          placeholder="Front of card..."
+                          defaultValue={this.props.cardModel.front}
+                          onChange={_.partial(this.handleChange, 'front')} />
+            </div>
+            <div className="form-group">
+                <label htmlFor="editor-input-back">Back</label>
+                <textarea id="editor-input-back" className="form-control span6"
+                          placeholder="Back of card..."
+                          defaultValue={this.props.cardModel.back}
+                          onChange={_.partial(this.handleChange, 'back')} />
+            </div>
+            <div className="form-group">
+                <label htmlFor="editor-input-tags">Tags</label>
+                <input type="text" id="editor-input-tags"
+                               className="tagsinput form-control"
+                               defaultValue={tagsCSV}
+                               ref="tagsinput" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="editor-input-source">Source URL</label>
+                <input type="text" id="editor-input-source" className="form-control"
+                       defaultValue={this.props.cardModel.source_url}
+                       onChange={_.partial(this.handleChange, 'source_url')} />
+            </div>
+            <div className="checkbox">
+                <label><input type="checkbox"
+                              defaultChecked={this.props.cardModel.reversible}
+                              onChange={_.partial(this.handleChange, 'reversible')}/> Reversible</label>
+            </div>
+            <div className="checkbox">
+                <label><input type="checkbox"
+                              defaultChecked={this.props.cardModel.public}
+                              onChange={_.partial(this.handleChange, 'public')}/> Public</label>
+            </div>
+            <input type="submit" className="btn btn-primary" value="Save" />
         </form>;
+    },
+    componentDidMount: function() {
+        $(this.refs.tagsinput.getDOMNode()).tagsInput({
+            onChange: this.handleTagsInputChange
+        });
     }
 });
 
@@ -64,7 +98,7 @@ var Editor = React.createClass({
         }
 
         // If we don't have the data yet, display a temp message.
-        return <div>
+        return <div className="editor">
             Please wait while your card data is fetched for editing. <br/>
             {this.props.params.cardKey}
         </div>;
